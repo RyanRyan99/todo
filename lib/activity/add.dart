6fd1298b85c2f196b5f46ecb1f4be76a6b330a8f
RyanRyan99 <ryansutrisna99@gmail.com>
2020-02-26 16:51:22 +0700

    26/02/2020 UI

--- lib/activity/add.dart
+++ lib/activity/add.dart
@@ -52,7 +52,13 @@ class _AddFormState extends State<AddForm> {
     return Scaffold(
       body: Container(
         height: double.infinity,
-        color: Colors.blue,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            colors: [Colors.blue[100], Colors.blue[500]],
+          ),
+        ),
         child: Stack(
           children: <Widget>[
             Padding(
@@ -76,9 +82,105 @@ class _AddFormState extends State<AddForm> {
              Padding(
               padding: const EdgeInsets.only(top: 100),
               child: Container(
+                width: double.infinity,
                 height: 500,
                 margin: EdgeInsets.all(15.0),
-                color: Colors.white30,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20),
+                      Container(
+                        width: double.infinity,
+                         child: TextField(
+                           cursorColor: Colors.blue,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.library_books, color: Colors.white,),
+                            border:InputBorder.none
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white30,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: double.infinity,
+                        child: FlatButton(
+                          onPressed: ()=>selectDateTime(context),
+                          child: Align(alignment: Alignment.centerLeft,child: Text(_dateText, style: TextStyle(fontSize: 19, color: Colors.white),)),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white30,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: double.infinity,
+                         child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.library_books, color: Colors.white,),
+                              border:InputBorder.none
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white30,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: double.infinity,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.library_books, color: Colors.white,),
+                              border:InputBorder.none
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white30,
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(top: 20, right: 10, bottom: 10),
+                              height: 30,
+                               child: RaisedButton(
+                                 color: Colors.blue,
+                                 child: Text("Save",style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),),
+                                 onPressed: (){},
+                                 shape: RoundedRectangleBorder(
+                                   borderRadius: BorderRadius.circular(20),
+                                   side: BorderSide(color: Colors.white30)
+                                 ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 20, right: 10, bottom: 10),
+                              height: 30,
+                              child: RaisedButton(
+                                color: Colors.blue,
+                                child: Text("No",style: TextStyle(color: Colors.white, fontFamily: "OpenSans"),),
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  side: BorderSide(color: Colors.white30)
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             )
           ],
